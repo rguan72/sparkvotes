6b121f18b4f01b98afdddc8d4a793c5a3f06ad52
Robert Buckley <robertbuckleybee2@gmail.com>
2020-10-04 17:24:41 -0400

    Failed animation

--- src/stories/Bar.js
+++ src/stories/Bar.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import { useSpring, animated } from "react-spring";
+import AnimateHeight from 'react-animate-height';
 import "./bar.css";
 
 /**
@@ -18,45 +18,46 @@ export const Bar = ({ continues, isCorrect, correctCaption, incorrectCaption, co
   //   console.log("yooo")
   //   setHeight({ height: "20vh" })
   // }, [setHeight])
-  const props = useSpring({ height: "20vh", from: {height: "0vh"}})
-  const AnimatedBox = animated(Box)
+  
+  const [ height, setHeight ] = useState("0vh")
+  useEffect(() => {
+    if (continues) setHeight("20vh");
+    else setHeight("0vh")
+  }, [continues])
   let correctChoicesMapped = correctChoices;
   if (correctChoices.length > 1) {
     correctChoicesMapped = correctChoices.map((item, idx) => { if (idx < correctChoices.length - 1) { return `${item}, ` } else { return item } })
   }
   return (
-    <AnimatedBox
-      mt={"3vh"}
-      position="absolute"
-      bottom={0}
-      left={0}
-      width="100vw"
-      height={props.height}
-      style = {{
-        backgroundColor: isCorrect ? "#d4e6ae" : "#cf6428",
-        zIndex: 2,
-        marginTop: "3vh",
-        position: "absolute",
-        bottom: 0,
-        left: 0,
-        width: "100vw",
-        height: "20vh",
-      }}
-    >
-      {continues && (
-        <Box ml={"5vw"} mt={"1vh"}>
-          {isCorrect ? <Typography variant="h5">{correctCaption}</Typography> : ""}
-        </Box>
-      )}
-      {continues && !isCorrect && (
-        <Box ml={"5vw"}>
-          <Typography variant="h4" style={{ color: "white" }}>Correct Answer:</Typography>
-          <Typography variant="h5">{correctChoicesMapped}</Typography>
-        </Box>
-      )}
-    </AnimatedBox>
+      <AnimateHeight
+        id="bottom-bar"
+        height={height}
+        duration={ 500 }
+        style={{
+          backgroundColor: isCorrect ? "#d4e6ae" : "#cf6428",
+          flexShrink: 0,
+          zIndex: 2, 
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          marginTop: "3vh",
+          width: "100vw"
+        }}
+      >
+        {continues && (
+          <Box ml={"5vw"} mt={"1vh"}>
+            {isCorrect ? <Typography variant="h5">{correctCaption}</Typography> : ""}
+          </Box>
+        )}
+        {continues && !isCorrect && (
+          <Box ml={"5vw"}>
+            <Typography variant="h4" style={{ color: "white" }}>Correct Answer:</Typography>
+            <Typography variant="h5">{correctChoicesMapped}</Typography>
+          </Box>
+        )}
+      </AnimateHeight>
   );
-};
+};  
 
 
 
